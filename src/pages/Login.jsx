35eed72cf1943eb2e5464d9e0e4aa75a051e3604
Erick Ribeiro <ribeiroerick2024@gmail.com>
2024-09-24 19:40:44 -0300

    First Commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,17 @@
+function Login() {
+    return (
+      <div className="container">
+        <form>
+          <div>
+            <input type="text" placeholder="Login" />
+          </div>
+          <div>
+            <input type="password" placeholder="Senha" />
+          </div>
+        </form>
+      </div>
+    );
+  }
+  
+  export default Login;
+  
