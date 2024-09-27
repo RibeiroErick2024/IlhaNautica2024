70e4ed4a40dd131c6cf7929e0edb1faa4171845d
Erick Ribeiro <ribeiroerick2024@gmail.com>
2024-09-27 13:26:10 -0300

    Parte visual quase pronta, falta alguns ajustes

--- login/src/App.jsx
+++ login/src/App.jsx
@@ -1,15 +1,12 @@
-
-import Login from './pages/Login'
-import './App.css'
+import "./App.css";
+import Login from "./pages/Login";
 
 function App() {
- 
   return (
-    <>
-      
+    <div className="App">
       <Login />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
--- login/src/pages/Login.jsx
+++ login/src/pages/Login.jsx
@@ -1,59 +1,62 @@
-import './Login.css'
+import { useState } from "react";
+
+import "./Login.css";
 
 const Login = () => {
+ 
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  
+  const handleSubmit = (event) => {
+    
+    event.preventDefault();
+
+    
+    console.log("Dados de Login:", { username, password });
+  };
+
   return (
-    
-      
-    <div className='container'>
-
-        <div className='ladoEsquerdo'>
-            <div className='img'>
-                <img src="src\assets\imgLogin.jpg" alt="" />
-            </div>
-
-
-
-
-
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <h1>Entrar</h1>
+        <div className="input-field">
+          <input
+            type="text"
+            placeholder="E-mail"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          
         </div>
-        <div className='ladoDireito'>
-
-        <div className='login'>
-        <form>
-
-            <h1>Entrar</h1>
-
-            <div>
-                <input type="email" name="" id=""  placeholder='Digite o seu email'/>
-            </div>
-
-            <div>
-                <input type="password" name="" id="" placeholder='Digite a sua senha' />
-            </div>
-
-            <label className='esqueceuSenha'>Esqueceu a sua senha</label>
-            <div>
-                <button className='btnEntrar'>Proximo</button>
-            </div>
-            {/* <label className='semConta'>Não tem uma conta?</label> */}
-
-
-        </form>
-
-
-
+        <div className="input-field">
+          <input
+            type="password"
+            placeholder="Senha"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          
         </div>
 
+        <div className="recall-forget">
+          {/* <label>
+            <input type="checkbox" />
+            Lembre de mim
+          </label> */}
+          <a href="#">Esqueceu sua senha?</a>
         </div>
-
-        
-
-        
-    
-
-
+        <button type="submit">Login</button>
+        <div className="signup-link">
+          <p>
+            Não tem uma conta? <a href="#">Registrar</a>{" "}
+          </p>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
