fff18d1cf1db053da208b54e399d33fcece99d81
Erick Ribeiro <ribeiroerick2024@gmail.com>
2024-10-03 21:55:59 -0300

    Login e cadastro Concluido

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import "./App.css";
 import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
+import RecuperacaoSenha from "./pages/RecuperacaoSenha";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} /> {/* Página de Login */}
+          <Route path="/cadastro" element={<Cadastro />} /> {/* Página de Cadastro */}
+          <Route path="/recuperacaoSenha" element={<RecuperacaoSenha />} /> {/* Página de recuperaçao de senha */}
+
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,57 +1,72 @@
 import { useState } from "react";
-
 import "./Login.css";
+import { Eye, EyeOff, Mail } from "lucide-react";
+
 
 const Login = () => {
- 
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
 
-  
+  const [usuario, setUsuario] = useState("");
+  const [senha, setSenha] = useState("");
+  const [mostrarSenha, setMostrarSenha] = useState(false);
+
+//Envio do formulario
   const handleSubmit = (event) => {
-    
     event.preventDefault();
+    console.log("Dados de Login:", { usuario, senha });
+  };
 
-    
-    console.log("Dados de Login:", { username, password });
+//Controla visibilidade da senha
+  const toggleSenhaVisibilidade = () => {
+    setMostrarSenha(!mostrarSenha);
   };
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
         <h1>Entrar</h1>
+
+
         <div className="input-field">
           <input
             type="text"
             placeholder="E-mail"
             required
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+            value={usuario}
+            onChange={(e) => setUsuario(e.target.value)}
           />
-          
-        </div>
-        <div className="input-field">
-          <input
-            type="password"
-            placeholder="Senha"
-            required
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          
+          <Mail size={22} className="icon"/>
         </div>
 
-        <div className="recall-forget">
-          {/* <label>
-            <input type="checkbox" />
-            Lembre de mim
-          </label> */}
-          <a href="#">Esqueceu sua senha?</a>
+
+        <div className="input-field" style={{ position: "relative" }}>
+          <input
+            type={mostrarSenha ? "text" : "password"}
+            placeholder="Senha"
+            required
+            value={senha} 
+            onChange={(e) => setSenha(e.target.value)}
+          />
+          
+
+          <div className="iconeSenha" onClick={toggleSenhaVisibilidade}>
+
+            {mostrarSenha ? (
+              <EyeOff size={22} />) : (<Eye size={22} />)
+            }
+          </div>
         </div>
+
+
+
+        <div className="recall-forget">
+          <a href="/recuperacaoSenha">Esqueceu sua senha?</a>
+        </div>
+
+
         <button type="submit">Login</button>
         <div className="signup-link">
           <p>
-            Não tem uma conta? <a href="#">Registrar</a>{" "}
+            Não tem uma conta? <a href="/cadastro">Registrar</a>
           </p>
         </div>
       </form>
