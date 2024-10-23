4f86b9c43f9852e2fdc2fb84f87a4365eef1a622
Gisele-Mara <ggisa.mj@gmail.com>
2024-10-23 19:53:45 -0300

    something

--- src/pages/CadastroDescricao.jsx
+++ src/pages/CadastroDescricao.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './CadastroDescricao.css';
+import axios from 'axios';
 
 function CadastroDescricao() {
     const [inputDescricao, setInputDescricao] = useState("");
@@ -22,93 +23,123 @@ function CadastroDescricao() {
     }, [fotosSelecionadas]);
 
     const handleUploadClick = () => {
-        // 👇 We redirect the click event onto the hidden input element
+        // 👇 Click para fazer upload das fotos do input que está escondido
         esconderInputFile.current?.click();
     };
 
 
     // Upload multiple photos
     function handleMultipleChange(event) {
-        const files = Array.from(event.target.files);
-        setFotosSelecionadas(files);
+        const fotos = Array.from(event.target.files);
+        setFotosSelecionadas(fotos);
     }
+
+    function handleSubmit(event) {
+        event.preventDefault();
+        const url = 'http://localhost:8080/';
+        const formData = new FormData();
+        fotosSelecionadas.forEach((file, index) => {
+          formData.append(`foto${index}`, file);
+        });
+    
+        const config = {
+          headers: {
+            'content-type': 'multipart/form-data',
+          },
+        };
+    
+        axios.post(url, formData, config)
+          .then((response) => {
+            console.log(response.data);
+            // setUploadedFiles(response.data.files);
+          })
+          .catch((error) => {
+            console.error("Error ao fazer upload de fotos: ", error);
+          });
+      }
 
     return (
         <div>
             <div className='container'>
-                <div className='mainTitulo'>
-                    <div className='tituloDesc'>
-                        <h2>Seu barco</h2>
-                        <hr className='linhaHr' />
-                    </div>
-                </div>
-
-                <div className='inputContainer'>
-                    <div className='descricaoContainer'>
-                        <div className='textoDesc'>
-                            <h4>Descreva sua embarcação</h4>
-                        </div>
-                        <div className='descriçaoInput'>
-                            <textarea
-                                className='descriçaoInput'
-                                value={inputDescricao}
-                                onChange={e => setInputDescricao(e.target.value)}
-                                type="text"
-                                aria-multiline={true}
-                                rows="7" cols="100"
-                             
-                                placeholder='Um barco lindo de 1000m²'
-                            />
+                <form onSubmit={handleSubmit}>
+                    <div className='mainTitulo'>
+                        <div className='tituloDesc'>
+                            <h2>Seu barco</h2>
+                            <hr className='linhaHr' />
                         </div>
                     </div>
-                    <div className='regrasContainer'>
-                        <div className='textoDesc'>
-                            <h4>Descreva suas regras</h4>
+
+                    <div className='inputContainer'>
+                        <div className='descricaoContainer'>
+                            <div className='textoDesc'>
+                                <h4>Descreva sua embarcação</h4>
+                            </div>
+                            <div className='descriçaoInput'>
+                                <textarea
+                                    className='descriçaoInput'
+                                    value={inputDescricao}
+                                    onChange={e => setInputDescricao(e.target.value)}
+                                    type="text"
+                                    aria-multiline={true}
+                                    rows="7" cols="130"
+
+                                    placeholder='Um barco lindo de 1000m²'
+                                />
+                            </div>
                         </div>
-                        <div className='regrasInput'>
-                            <textarea
-                                value={inputRegras}
-                                onChange={e => setInputRegras(e.target.value)}
-                                type="text"
-                                aria-multiline={true}
-                                rows="4" cols="50"
-                                placeholder='Descreva as regras da embarção'
-                            />
+                        <div className='regrasContainer'>
+                            <div className='textoDesc'>
+                                <h4>Descreva suas regras</h4>
+                            </div>
+                            <div className='regrasInput'>
+                                <textarea
+                                    value={inputRegras}
+                                    onChange={e => setInputRegras(e.target.value)}
+                                    type="text"
+                                    aria-multiline={true}
+                                    rows="7" cols="100"
+                                    placeholder='Descreva as regras da embarcação'
+                                />
+                            </div>
                         </div>
                     </div>
-                </div>
 
-                <div className='fotosContainer'>
-                    <div className='tituloDesc'>
-                        <h2>Fotos</h2>
-                        <hr className='linhaHr' />
-                    </div>
-                    <div className='inputFotos'>
-                        <input type="file"
-                            multiple
-                            ref={esconderInputFile}
-                            style={{ display: 'none' }}
-                            onChange={handleMultipleChange} />
+                    <div className='fotosContainer'>
+                        <div className='tituloDesc'>
+                            <h2>Fotos</h2>
+                            <hr className='linhaHr' />
+                        </div>
+                        <div className='inputFotos'>
+                            <input type="file"
+                                accept="image/*"
+                                multiple
+                                ref={esconderInputFile}
+                                style={{ display: 'none' }}
+                                onChange={handleMultipleChange} />
 
-                        <div className='imagesList'>
-                            <button className='btnSelecionar'
-                                onClick={handleUploadClick}>
-                                "Click to select"
-                            </button>
-                           <div className='imagesList'>
-                            {previews.map((preview, index) => (
-                                <img className='imageSelecionada' key={index} src={preview} />
-                            ))}
-                           </div>
+                            <div className='imagesList'>
+                                <button className='btnSelecionar'
+                                    onClick={handleUploadClick}>
+                                    "Click to select"
+                                </button>
+                                <div className='imagesList'>
+                                    {previews.map((preview, index) => (
+                                        <img className='imageSelecionada' key={index}
+                                            src={preview}
+                                        />
+                                    ))}
+                                </div>
+                            </div>
+
+                        </div>
+                        <div>
+
+
                         </div>
 
                     </div>
-                    <div>
-
-
-                    </div>
-
-                </div>
+                    {/* <button type="submit" className="btnAnuncie">Anuncie</button> */}
+                </form>
             </div>
         </div>
     );
