cc0592e59c07d9d917081790fb0eea4f106397e3
Gisele-Mara <ggisa.mj@gmail.com>
2024-10-31 21:56:01 -0300

    imagens adicionadas post e get

--- src/components/FormCadastroDescricao/index.jsx
+++ src/components/FormCadastroDescricao/index.jsx
@@ -4,16 +4,72 @@ import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
+
 function CadastroDescricao() {
     const [inputDescricao, setInputDescricao] = useState("");
     const [inputRegras, setInputRegras] = useState("");
     const [fotosSelecionadas, setFotosSelecionadas] = useState([]);
     const [previews, setPreviews] = useState([]);
+    const [imageSrc, setImageSrc] = useState([]);
+
+
+    // const loadEvent = async (imageIds) => {
+    //     try {
+    //         const imagePromises = imageIds.map(async (id) => {
+    //             const result = await axios.get(`http://localhost:8080/imagem/${id}`, { responseType: 'arraybuffer' });
+    //             const base64String = btoa(
+    //                 new Uint8Array(result.data)
+    //                     .reduce((data, byte) => data + String.fromCharCode(byte), '')
+    //             );
+    //             return `data:image/png;base64,${base64String}`;
+    //         });
+
+    //         // Aguarda todas as promessas serem resolvidas
+    //         const images = await Promise.all(imagePromises);
+    //         setImageSrc(images); // Atualiza o estado com um array de imagens
+    //     } catch (error) {
+    //         console.error("Erro ao carregar as imagens:", error);
+    //     }
+    // };
+    const loadEvent = async () => {
+        try {
+            const result = await axios.get(`http://localhost:8080/imagem/11`, { responseType: 'arraybuffer' });
+            const base64String = btoa(
+                new Uint8Array(result.data)
+                    .reduce((data, byte) => data + String.fromCharCode(byte), '')
+            );
+            const formato = result.headers['content-type']
+            console.log(result.headers['content-type'])
+            setImageSrc(`data:${formato};base64,${base64String}`);
+        } catch (error) {
+            console.error("Erro ao carregar a imagem:", error);
+        }
+    };
+
+    // const loadEvent = async () => {
+    //     try {
+    //         const result = await axios.get('http://localhost:8080/imagem/8', { responseType: 'arraybuffer' });
+            
+    //         // Supondo que o resultado seja um array de imagens em formato arraybuffer
+    //         const base64Images = result.data.map(imageBuffer => {
+    //             const base64String = btoa(
+    //                 new Uint8Array(imageBuffer)
+    //                     .reduce((data, byte) => data + String.fromCharCode(byte), '')
+    //             );
+    //             return `data:image/svg;base64,${base64String}`;
+    //         });
+    
+    //         setImageSrc(base64Images); // Atualiza o estado com um array de imagens
+    //     } catch (error) {
+    //         console.error("Erro ao carregar as imagens:", error);
+    //     }
+    // };
 
     //Para esconder o input file de origem
     const esconderInputFile = useRef(null);
     useEffect(() => {
-
+        loadEvent();
         // URL representando o arquivo (foto)
         const newPreviews = fotosSelecionadas.map(file => URL.createObjectURL(file));
         setPreviews(newPreviews);
@@ -44,10 +100,11 @@ function CadastroDescricao() {
 
     function handleSubmit(event) {
         event.preventDefault();
-        const url = 'http://localhost:8080/';
+        const url = 'http://localhost:8080/imagem/upload';
         const formData = new FormData();
         fotosSelecionadas.forEach((file, index) => {
-            formData.append(`foto${index}`, file);
+            console.log("Appending file:", file);
+            formData.append(`imagens`, file);
         });
 
         const config = {
@@ -150,6 +207,11 @@ function CadastroDescricao() {
                                             src={preview}
                                         />
                                     ))}
+                                    {/* {imageSrc.map((src, index) => (
+                                        <img key={index} src={src} alt={`Imagem ${index}`} />
+                                    ))} */}
+                                    {imageSrc && <img src={imageSrc} alt="Imagem carregada" />}
+
                                 </div>
                             </div>
 
