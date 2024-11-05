45c84e481966c0012095a6e9f74fd51b9ab1916b
ANDREY FREITAS LOPES <andrey_lopes@estudante.sesisenai.org.br>
2024-11-05 19:30:41 -0300

    embarcacao

--- src/components/EmbarcacaoGaleria/index.jsx
+++ src/components/EmbarcacaoGaleria/index.jsx
@@ -3,16 +3,25 @@ import './index.css'; // Importe seu CSS
 
 function EmbarcacaoGaleria({ fotos }) {
   return (
-    <div className="galeria-container">
-      <div className="div-img-grande">
-        <img src={fotos[0]} alt="Imagem Grande" className="foto-grande" />
+    <div className='container-fotos'>
+      <div className="div-fabricante-modelo-embarcacao">
+        <h2>Fabricante - modelo</h2>
       </div>
-      <div className="div-img-pequena">
-        {fotos.slice(1).map((foto, index) => (
-          <div key={index} className="fotos">
-            <img src={foto} alt={`Imagem Pequena ${index + 1}`} className="foto-pequena" />
-          </div>
-        ))}
+      <div className="galeria-container">
+        <div className="div-img-grande">
+          <img src={fotos[0]} alt="Imagem Grande" className="foto-grande" />
+        </div>
+        <div className="div-img-pequena">
+          {fotos.slice(1).map((foto, index) => (
+            <div key={index} className="fotos">
+              <img src={foto} alt={`Imagem Pequena ${index + 1}`} className="foto-pequena" />
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="div-usuario-embarcacao">
+        <h2>é us guri</h2>
       </div>
     </div>
   );
