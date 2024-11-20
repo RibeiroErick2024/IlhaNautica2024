e405955a070f523598a5024fe25342e9fd30a83a
ANDREY FREITAS LOPES <andrey_lopes@estudante.sesisenai.org.br>
2024-11-19 21:56:49 -0300

    gisele não me deixou fazer nada

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,7 +10,7 @@ function Home() {
         <Header/>
       <div className='conteudo-container'>
         <Filtros/>
-        <Card_barco/>
+        <ListagemBarcos/>
       </div>
     </div>
   )
